cc2c52409553d9eedfaa7ba2a8a61734a85e41dd
rafa9694 <rafa9694@gmail.com>
2021-11-11 20:16:28 -0300

    Create Project

---
+++ src/main/java/com/rafadev/cdbackend/CdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rafadev.cdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafadev/cdbackend/CdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafadev.cdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
